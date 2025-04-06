092e30ce0cc5c7f32ea42b8e443f5218cebf2a1c
Tanapat <a_little_kids@hotmail.com>
2025-04-06 15:42:09 +0700

    publish message after customer created

--- kafka-apache/src/main/java/com/example/kafka_apache/controller/CustomerController.java
+++ kafka-apache/src/main/java/com/example/kafka_apache/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.example.kafka_apache.controller;
 import com.example.kafka_apache.model.Customer;
 import com.example.kafka_apache.service.CustomerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -13,11 +14,13 @@ import reactor.core.publisher.Mono;
 public class CustomerController {
 
     private final CustomerService customerService;
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
     @PostMapping("")
     public Mono<Customer> create(@RequestBody Customer customer)
     {
-        return customerService.save(customer);
+        return customerService.save(customer)
+                .flatMap(this::notifyCustomerCreated);
     }
 
     @GetMapping("/{id}")
@@ -30,5 +33,15 @@ public class CustomerController {
     public Flux<Customer> getAll()
     {
         return customerService.findAll();
+    }
+
+    private Mono<Customer> notifyCustomerCreated(Customer customer)
+    {
+        return Mono.fromFuture(
+                kafkaTemplate.send(
+                        "test-topic",
+                        "customer-created",
+                        String.format("customer has been created [id = %s]", customer.getId()))
+        ).map(x -> customer);
     }
 }
