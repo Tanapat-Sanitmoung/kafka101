780f2863f31f30167fece3b9e7d5b9a2d498a373
Tanapat <a_little_kids@hotmail.com>
2025-04-06 12:30:13 +0700

    add consumer

--- kafka-apache/src/main/java/com/example/kafka_apache/SimpleController.java
+++ kafka-apache/src/main/java/com/example/kafka_apache/SimpleController.java
@@ -1,5 +1,7 @@
 package com.example.kafka_apache;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SimpleController {
 
+    final static Logger LOGGER = LoggerFactory.getLogger(SimpleController.class);
+
     @Autowired
     KafkaTemplate<String, String> kafkaTemplate;
 
@@ -17,6 +21,8 @@ public class SimpleController {
     @PostMapping("/publish")
     public void publishMessage(@RequestParam String msg)
     {
-        this.kafkaTemplate.send(TOPIC, msg);
+        this.kafkaTemplate.send(TOPIC, "msg", msg).join();
+
+        LOGGER.info("Publish message : {}", msg);
     }
 }
---
+++ kafka-apache/src/main/java/com/example/kafka_apache/service/SimpleConsumer.java
@@ -0,0 +1,21 @@
+package com.example.kafka_apache.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleConsumer {
+
+    final static Logger LOGGER = LoggerFactory.getLogger(SimpleConsumer.class);
+
+    @KafkaListener(
+            id = "hai-mae",
+            topics = "test-topic",
+            autoStartup = "true")
+    public void listen(String msg)
+    {
+        LOGGER.info("Get message : {}", msg);
+    }
+}
